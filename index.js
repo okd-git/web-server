0a66645f00c116c93e26a598d93fb9dd05ad0972
okd <xxxxxx@gmail.com>
2022-06-09 23:21:27 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path = require("path");
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get('/', function (req, res) {
+//     console.log(req);
+//     res.send('')
+// })
+
+app.post('/api/v1/quiz', function (req, res) {
+    const answer = req.body.answer;
+    if (answer === "2") {
+        res.redirect("/correct.html");
+    } else {
+        res.redirect("/wrong.html");
+    }
+});
+
+app.get('/api/v1/users', function (req, res) {
+    res.send({
+        name: "Mike",
+        age: 30
+    });
+});
+
+app.listen(3000, function () {
+    console.log("I am running");
+});
+
+console.log("最終行");
